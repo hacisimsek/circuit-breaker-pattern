62bcc562b22f8fd9130f2af18ffa293e1dc7981f
Hacı Şimşek <haci.simsek@sisal.com>
2024-09-03 21:30:16 +0300

    init commit

---
+++ src/test/java/com/hacisimsek/circuitBreakerApp/CircuitBreakerPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hacisimsek.circuitBreakerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitBreakerPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
